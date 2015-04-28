cc2bc5dbf1a812d8403048fae6843fbee39acf92
samjwalston <samjwalston@gmail.com>
2015-04-28 11:56:11 -0700

    finished the mobile navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,57 +17,88 @@
 $(document).ready(function(){
 	// HTML Elements
 	var $logo = $('#logo');
-	var $nav = $('#nav_list');
-	var $reveal = $('#nav_reveal_cont');
+	var $navWeb = $('#nav_list_web');
+	var $revealWeb = $('#nav_reveal_web');
+	var $navMobile = $('#nav_list_mobile');
+	var $revealMobile = $('#nav_reveal_mobile');
 
 	// Scroll Manipulations
 	$(window).scroll(function () {
-		// Class Manipulation for the logo animations.
-		// Scroll Down
-		if ($(this).scrollTop() < 285 && $logo.hasClass('invisible') == false) {
-			$logo.addClass('fadeOutUp');
-			$logo.removeClass('fadeInDown');
-			setTimeout(function() {
-      		$logo.addClass('invisible');
-			}, 500);
-		}
-		// Scroll Up
-		if ($(this).scrollTop() > 285) {
-			$logo.removeClass('invisible fadeOutUp');
-			$logo.addClass('fadeInDown');
-		}
+		// Website Navbar Scroll Animations. Only works if the browser width is > 640px
+		if ($(this).width() > 640) {
+			// Class Manipulation for the Navbar Logo animations.
+			// Scroll Down
+			if ( ($(this).width() < 1024 && $(this).scrollTop() < 200 && $logo.hasClass('invisible') == false) ||
+					 ($(this).width() > 1024 && $(this).scrollTop() < 240 && $logo.hasClass('invisible') == false) ) {
+				$logo.addClass('fadeOutUp');
+				$logo.removeClass('fadeInDown');
+				setTimeout(function() {
+    	  		$logo.addClass('invisible');
+				}, 500);
+			}
+			// Scroll Up
+			if ( ($(this).width() < 1024 && $(this).scrollTop() > 200) ||
+					 ($(this).width() > 1024 && $(this).scrollTop() > 240) ) {
+				$logo.removeClass('invisible fadeOutUp');
+				$logo.addClass('fadeInDown');
+			}
 
-		// Class Manipulation for navbar animations
-		// Scroll Down
-		if ($(this).scrollTop() < 440 && $reveal.hasClass('hidden') == false) {
-			revealNav();
-		}
-		// Scroll Up
-		if ($(this).scrollTop() > 440 && $nav.hasClass('hidden') == false) {
-			$reveal.removeClass('hidden fadeOutUp');
-    	$reveal.addClass('fadeInDown');
-			$nav.addClass('fadeOutUp');
-			$nav.removeClass('fadeInDown');
-			setTimeout(function() {
-    		$nav.addClass('hidden');
-			}, 250);
+			// Class Manipulation for Navbar animations
+			// Scroll Down
+			if ( ($(this).width() < 1024 && $(this).scrollTop() < 340 && $revealWeb.hasClass('hidden') == false) ||
+					 ($(this).width() > 1024 && $(this).scrollTop() < 395 && $revealWeb.hasClass('hidden') == false) ) {
+				revealNav();
+			}
+			// Scroll Up
+			if ( ($(this).width() < 1024 && $(this).scrollTop() > 400 && $navWeb.hasClass('hidden') == false) ||
+					 ($(this).width() > 1024 && $(this).scrollTop() > 450 && $navWeb.hasClass('hidden') == false) ) {
+				$revealWeb.removeClass('hidden fadeOutUp');
+    		$revealWeb.addClass('fadeInDown');
+				$navWeb.addClass('fadeOutUp');
+				$navWeb.removeClass('fadeInDown');
+				setTimeout(function() {
+    			$navWeb.addClass('hidden');
+				}, 250);
+			}
 		}
 	});
 
-	// Wrapped this section in a function because it's called again on click of the 'reveal_nav' button
+	// Wrapped this section in a function because it's called again on click of the '$revealWeb' button
 	function revealNav() {
-		$nav.removeClass('hidden fadeOutUp');
-		$nav.addClass('fadeInDown');
-		$reveal.addClass('fadeOutUp');
-		$reveal.removeClass('fadeInDown');
+		$navWeb.removeClass('hidden fadeOutUp');
+		$navWeb.addClass('fadeInDown');
+		$revealWeb.addClass('fadeOutUp');
+		$revealWeb.removeClass('fadeInDown');
 		setTimeout(function() {
-     	$reveal.addClass('hidden');
+     	$revealWeb.addClass('hidden');
 		}, 250);
 	}
 
-	// Reveals the NavBar again when scrolled down on the page
-	$('#nav_reveal').click(function() {
+	// Reveals the Navbar again when scrolled down on the page
+	$revealWeb.click(function() {
 		revealNav();
 	});
+
+	// Opens the Mobile Navbar
+	$revealMobile.click(function() {
+		$revealMobile.removeClass('fadeInLeft');
+		$revealMobile.addClass('fadeOutLeft');
+		setTimeout(function() {
+     	$revealMobile.addClass('hidden');
+     	$navMobile.removeClass('hidden fadeOutLeft');
+     	$navMobile.addClass('fadeInLeft');
+		}, 250);
+	})
+
+	// Closes the Mobile Navbar
+	$('#nav_mobile_close').click(function() {
+		$navMobile.removeClass('fadeInLeft');
+		$navMobile.addClass('fadeOutLeft');
+		setTimeout(function() {
+     	$navMobile.addClass('hidden');
+     	$revealMobile.removeClass('hidden fadeOutLeft');
+     	$revealMobile.addClass('fadeInLeft');
+		}, 250);
+	})
 
 });
