6ef69b617e39c80bb0175608eb63afe0e05eb9ca
samjwalston <samjwalston@gmail.com>
2015-06-02 12:23:29 -0700

    slider test

--- public/assets/application-a5d5ab335db6a4684f2ec8d12c4a2a18da3d977ec17864c7e770f6cf2f2c7eae.js
+++ public/assets/application-a5d5ab335db6a4684f2ec8d12c4a2a18da3d977ec17864c7e770f6cf2f2c7eae.js
@@ -10883,7 +10883,10 @@ $(document).ready(function() {
     bodyWidth = document.body.clientWidth;
     header.style.height = (bodyWidth / 3) + 'px';
 
-    if (bodyWidth) { jssor_slider1.$ScaleWidth(Math.min(bodyWidth, 1920)); }
+    if (bodyWidth) {
+      jssor_slider1.$ScaleWidth(Math.min(bodyWidth, 1920));
+      jssor_slider1.$ScaleHeight(Math.min((bodyWidth / 3), 640));
+    }
     else { window.setTimeout(ScaleSlider, 30); }
   }
 
