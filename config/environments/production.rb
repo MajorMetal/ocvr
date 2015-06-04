3085df0545ea3af2881690133d2e2a0d8d087c9f
samjwalston <samjwalston@gmail.com>
2015-06-04 11:23:43 -0700

    third compresssion test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   config.assets.compress = true
   config.assets.compile = true
   config.assets.digest = true
-  config.static_cache_control = "public, max-age=60"
+  config.static_cache_control = "public, max-age=31536000"
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
