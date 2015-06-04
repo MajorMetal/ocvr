f0e95d18fada881b1537f9ab179f498b14e07c31
samjwalston <samjwalston@gmail.com>
2015-06-04 12:34:48 -0700

    max age update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   config.assets.compress = true
   config.assets.compile = true
   config.assets.digest = true
-  config.static_cache_control = "public, max-age=86400"
+  config.static_cache_control = "public, max-age=604800"
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
